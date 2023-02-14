f7875268ede2129e50381c265eb14aeba77161c7
Valdiney Daniel Garcia <valdineysc@gmail.com>
2023-02-13 23:56:25 -0300

    Primeira versão sem1_exercicio2

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿double alturaTriangulo, baseTriangulo;
+Console.WriteLine("Informe a altura do triangulo (em cm):");
+alturaTriangulo = double.Parse(Console.ReadLine());
+Console.WriteLine("Informe a base do triangulo (em cm):");
+baseTriangulo = double.Parse(Console.ReadLine());
+double areaTriangulo = (baseTriangulo * alturaTriangulo) / 2;
+Console.WriteLine($"A área do triangulo é de {areaTriangulo} cm.");
+
